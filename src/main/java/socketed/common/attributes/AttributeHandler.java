e2d3dd7ce569d1e763a4b61681cb6cdbd310e055
nischhelm <nischhelm@gmail.com>
2026-03-02 18:28:26 +0100

    fix trade cost attribute
    now only reduces either first OR second buy item count.
    now doesnt keep the reduced prices 💀
    now only does that on server side (idek why this event even happens on clientside ngl)

--- src/main/java/socketed/common/attributes/AttributeHandler.java
+++ src/main/java/socketed/common/attributes/AttributeHandler.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.village.MerchantRecipe;
 import net.minecraft.village.MerchantRecipeList;
 import net.minecraftforge.event.entity.EntityEvent;
@@ -33,6 +34,7 @@ public class AttributeHandler {
         if(amount <= 0) return;
 
         IAttributeInstance xpAttribute = player.getEntityAttribute(SocketedAttributes.XP);
+        if(xpAttribute == null) return;
         xpAttribute.setBaseValue(amount);
         event.setDroppedExperience((int) xpAttribute.getAttributeValue());
         xpAttribute.setBaseValue(0);
@@ -42,21 +44,40 @@ public class AttributeHandler {
     public static void onTradeOffers(MerchantTradeOffersEvent event){
         EntityPlayer player = event.getPlayer();
         if(player == null) return;
+        if(player.world.isRemote) return; //why does this even run on client every single tick breh
         MerchantRecipeList trades = event.getList();
         if (trades == null) return;
-        //TODO: this doesnt work cause it fires multiple times for whatever reason
+
         IAttributeInstance tradeAttribute = player.getEntityAttribute(SocketedAttributes.TRADECOST);
-        for (MerchantRecipe recipe : event.getList()){
+        if(tradeAttribute == null || tradeAttribute.getModifiers().isEmpty()) return;
+
+        for (int i = 0; i < trades.size(); i++) {
+            MerchantRecipe recipe = trades.get(i);
             if(recipe.getItemToBuy().getCount() > 1)
-                reduceCost(recipe.getItemToBuy(), tradeAttribute);
-            if(recipe.hasSecondItemToBuy() && recipe.getSecondItemToBuy().getCount() > 1)
-                reduceCost(recipe.getItemToBuy(), tradeAttribute);
+                recipe = reduceCost(recipe, tradeAttribute, true);
+            else if(recipe.hasSecondItemToBuy() && recipe.getSecondItemToBuy().getCount() > 1)
+                recipe = reduceCost(recipe, tradeAttribute, false);
+            trades.set(i, recipe);
         }
     }
 
-    private static void reduceCost(ItemStack itemToBuy, IAttributeInstance tradeAttribute){
+    private static MerchantRecipe reduceCost(MerchantRecipe recipe, IAttributeInstance tradeAttribute, boolean targetsFirstItem){
+        MerchantRecipe newRecipe = recipe;
+
+        ItemStack itemToBuy = getStack(recipe, targetsFirstItem);
+
         tradeAttribute.setBaseValue(itemToBuy.getCount());
-        itemToBuy.setCount((int) Math.round(tradeAttribute.getAttributeValue()));
-        tradeAttribute.setBaseValue(0);
+        int newCount = MathHelper.clamp((int) Math.round(tradeAttribute.getAttributeValue()), 1, itemToBuy.getMaxStackSize());
+
+        if (newCount != itemToBuy.getCount()) {
+            newRecipe = new MerchantRecipe(recipe.writeToTags()); //copy to not modify the original trade
+            getStack(newRecipe, targetsFirstItem).setCount(newCount); //modify count
+        }
+        tradeAttribute.setBaseValue(0); //reset attribute, technically not needed
+        return newRecipe;
+    }
+
+    private static ItemStack getStack(MerchantRecipe recipe, boolean targetsFirstItem){
+        return targetsFirstItem ? recipe.getItemToBuy() : recipe.getSecondItemToBuy();
     }
 }
